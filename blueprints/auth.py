d66d980ac8fbb208a6db5ded23470eed3e6d216a
Jacqueline0122 <89665099+Jacqueline122@users.noreply.github.com>
2023-12-18 21:26:11 -0600

    setup email sent and verification code

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, render_template
+from exts import mail
+from flask_mail import Message
+from flask import request
+import string
+import random
 
 # /auth
 bp = Blueprint("auth", __name__, url_prefix="/auth")
@@ -12,3 +17,25 @@ def login_page():
 @bp.route("/register")
 def register_page():
     return render_template("register.html")
+
+
+@bp.route("/captcha/email")
+def captcha_email():
+    email = request.args.get("email")
+    source = string.digits*4
+    captcha = random.sample(source, 4)
+    print(captcha)
+    captcha = "".join(captcha)
+    message = Message(subject="InforSphere register verification code", recipients=[email], body=f"Your verification code is:{captcha} ")
+    mail.send(message)
+    
+    return "success"
+
+
+
+
+@bp.route("/mail/test")
+def mail_test():
+    message = Message(subject="email test", recipients=["long_j1@denison.edu"], body="This is test")
+    mail.send(message)
+    return "sent successfully"
--- config.py
+++ config.py
@@ -5,3 +5,13 @@ USERNAME = 'root'
 PASSWORD = 'longjiayu991216'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
+
+
+#pmqyeacqyjwxbebh
+
+MAIL_SERVER = "smtp.qq.com"
+MAIL_USE_SSL = True
+MAIL_PORT = 465
+MAIL_USERNAME = "605889046@qq.com"
+MAIL_PASSWORD = "pmqyeacqyjwxbebh"
+MAIL_DEFAULT_SENDER = "605889046@qq.com"
