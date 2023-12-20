ff2c43aefbf3591adbc51d1bef58e2a7c9d7ce0e
Jacqueline0122 <89665099+Jacqueline122@users.noreply.github.com>
2023-12-20 16:03:51 -0600

    add countdown after sent verification code

--- static/js/register.js
+++ static/js/register.js
@@ -1,6 +1,7 @@
-// after the website is loaded
-$(function(){
-    $("#captcha-btn").click(function (event) {
+function bindEmailCaptchaClick(){
+     $("#captcha-btn").click(function (event) {
+        // jquery object
+        var $this = $(this);
         event.preventDefault();
 
         var email = $("input[name='email']").val();
@@ -10,7 +11,22 @@ $(function(){
             success:function (result) {
                 var code = result['code'];
                 if (code === 200){
-                    alert("sent successfully");
+                    var countdown = 5;
+                    // make the button unclickable
+                    $this.off("click")
+                    var timer = setInterval(function () {
+                        $this.text(countdown);
+                        countdown -= 1;
+                        // after the timer is done
+                        if (countdown <= 0){
+                            // clear the timer
+                            clearInterval(timer);
+                            $this.text("Get Code");
+                            bindEmailCaptchaClick();
+                        }
+
+                    }, 1000);
+                    // alert("sent successfully");
                 }else{
                     alert(result['message']);
                 }
@@ -21,4 +37,9 @@ $(function(){
 
         })
     });
+}
+
+// after the website is loaded
+$(function(){
+    bindEmailCaptchaClick()
 })
